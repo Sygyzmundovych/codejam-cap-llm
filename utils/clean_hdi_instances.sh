2d44d1a59035c64f509c2b32298f220afe1fb033
Vitaliy Rudnytskiy <witalij.rudnicki@sap.com>
2025-04-24 21:26:29 +0200

    Refactor clean_hdi_instances.sh: improve space_guid retrieval and enhance instance deletion output

--- utils/clean_hdi_instances.sh
+++ utils/clean_hdi_instances.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-cf space $(cf target | grep 'space:' | awk '{print $2}') --guid
-# cf target | grep -E 'org:|space:' && echo "guid: $(cf space $(cf target | grep 'space:' | awk '{print $2}') --guid)"
+cf target | grep -E 'org:|space:' && echo "space_guid: $(cf space $(cf target | grep 'space:' | awk '{print $2}') --guid)"
 
-echo -n "HDI instances to be removed: "; cf curl "/v3/service_instances?service_plan_names=hdi-shared&space_guids=cb8de26a-a8f1-43d7-b0c9-f4c12f5a5fd0" | jq '.pagination.total_results'
-cf curl "/v3/service_instances?service_plan_names=hdi-shared&space_guids=cb8de26a-a8f1-43d7-b0c9-f4c12f5a5fd0&per_page=5000" | jq -c '.resources[] | {guid: .guid, name: .name}' | while read -r instance; do
-    read name guid < <(echo "$instance" | jq -r '.name, .guid')
-    echo "Deleting HDI: $name"
+space_guid=$(cf space $(cf target | grep 'space:' | awk '{print $2}') --guid)
+echo -n "HDI instances to be removed: "; cf curl "/v3/service_instances?service_plan_names=hdi-shared&per_page=100&space_guids="$space_guid | jq '.pagination.total_results'
+
+cf curl "/v3/service_instances?service_plan_names=hdi-shared&per_page=100&space_guids=$space_guid" | jq -c '.resources[] | {guid: .guid, name: .name}' | while read -r instance; do
+    read guid name < <(echo "$instance" | jq -r '"\(.guid) \(.name)"')
+    echo "Deleting $name with guid $guid"
     cf curl -X DELETE "/v3/service_instances/"$guid
+    # cf curl "/v3/service_instances/?names="$name | jq '.resources[].last_operation'
+    cf curl "/v3/service_instances/"$guid | jq '.last_operation'
 done
